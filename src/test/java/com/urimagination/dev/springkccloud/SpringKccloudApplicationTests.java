a9dd015789dd1bc2fea8c00e7fa248d9af2a9fbc
siralfbaez <siralf@urimagination.com>
2023-05-05 12:19:25 -0400

    Kafka 2st Commit Streams

---
+++ src/test/java/com/urimagination/dev/springkccloud/SpringKccloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urimagination.dev.springkccloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKccloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
